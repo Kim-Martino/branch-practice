69de5b923db315c64faa8652d20852b27b7eec53
Kim-Martino <kjw980621@gmail.com>
2022-08-18 11:38:03 +0900

    fear: Print Buzz
    condition : rename i to j, max index to 20, add FizzBuzz if i is times
              of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,7 @@
-for i in range(1,16+1):
-    if (i % 3 == 0):
+for j in range(1,20+1):
+    if (j % 15 == 0):
+        print("FizzBuzz")
+    elif (j % 3 == 0):
         print("Fizz")
-
     else:
-        print(i)
+        print(j)
